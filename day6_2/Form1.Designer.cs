cd5babf910ae114d8a448ef8be8894111a555b31
ezraeffect <build@fx0.kr>
2025-04-28 13:39:36 +0900

    add:array 실습

--- day6_2/Form1.Designer.cs
+++ day6_2/Form1.Designer.cs
@@ -28,13 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("굴림", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(776, 426);
+            this.textBox1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- day6_2/Form1.cs
+++ day6_2/Form1.cs
@@ -15,6 +15,28 @@ namespace day6_2
         public Form1()
         {
             InitializeComponent();
+
+            string[,] korean = {{"가","나","다"},
+                                {"라","마","바"},
+                                {"사","아","자"}};
+
+            textBox1.Text = korean[1, 2];
+
+            int[,,] nums = new int[,,]
+            {
+                {
+                    {1, 2, 3},
+                    {4, 5, 6}
+                },
+                {
+                    {7, 8, 9},
+                    {10, 11, 12}
+                }
+            };
+
+            textBox1.Text += nums[1,0,1];
+
+            
         }
     }
 }
